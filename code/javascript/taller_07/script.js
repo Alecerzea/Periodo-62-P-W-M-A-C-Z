9923d6674345c4c9c781fdfa7452e870385997c6
Alecerzea <85199063+Alecerzea@users.noreply.github.com>
2023-06-28 20:46:24 -0500

    main

--- code/javascript/taller_07/script.js
+++ code/javascript/taller_07/script.js
@@ -1,3 +1,5 @@
-function loadImage(imageUrl) {
-    document.getElementById('image').src = imageUrl;
-  }
+var div_cajita = document.getElementById('cajita')
+
+div_cajita.addEventListener('mousedown'), function(valor){
+  
+}
