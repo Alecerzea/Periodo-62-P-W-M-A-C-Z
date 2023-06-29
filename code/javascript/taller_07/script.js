3f88f624adeee2a68cab97ed63ea98cb86a5c626
Alecerzea <85199063+Alecerzea@users.noreply.github.com>
2023-06-28 21:43:29 -0500

    main

--- code/javascript/taller_07/script.js
+++ code/javascript/taller_07/script.js
@@ -1,27 +1,9 @@
 var div_cajita= document.getElementById('cajita')
-var es_presionado= false
-var offset= [0, 0]
+setTimeout(()=> {
+    div_cajita.style.left= (div_cajita.style.left +10) + 'px'
 
-div_cajita.addEventListener('mousedown', function(valor){
-    es_presionado= true
+},2000);
 
-    offset= [div_cajita.offsetLeft - valor.clientX, div_cajita.offsetTop - valor.clientY]
-
-    console.log(es_presionado)
-}, true)
-
-div_cajita.addEventListener('mouseup', function(valor){
-    es_presionado= false
-
-    console.log(es_presionado)
-}, true)
-
-div_cajita.addEventListener('mousemove', function(valor){
-    valor.preventDefault()
-    if(es_presionado) {
-        div_cajita.style.left= (valor.clientX + offset[0]) + 'px'
-        div_cajita.style.top= (valor.clientY + offset[1]) + 'px'
-    }
-    
-    console.log('[mousemove]')
-}, true)
+setTimeout(()=> {
+    div_cajita.style.left= (div_cajita.style.left +10) + 'px'
+},6000);
