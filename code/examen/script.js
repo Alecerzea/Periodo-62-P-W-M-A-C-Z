8ed8d9b26f05246b69f1a460c5b703dbd179a4c2
Alecerzea <85199063+Alecerzea@users.noreply.github.com>
2023-07-06 21:45:34 -0500

    main

--- code/examen/script.js
+++ code/examen/script.js
@@ -3,10 +3,10 @@ function showBookForm() {
    
     var formHTML = `
       <div class="form-container">
-        <label class="form-label" for="isbn">ISBN:</label>
-        <input class="form-input" type="text" id="isbn" name="isbn">
-        <label class="form-label" for="titulo">Título:</label>
-        <input class="form-input" type="text" id="titulo" name="titulo">
+        <label class="form-label" for="ISBN">ISBN:</label>
+        <input class="form-input" type="text" id="ISBN" name="ISBN">
+        <label class="form-label" for="titulo">Titulo:</label>
+        <input class="form-input" type="text" id="Titulo" name="Titulo">
         <label class="form-label" for="subtitulo">Subtítulo:</label>
         <input class="form-input" type="text" id="subtitulo" name="subtitulo">
         <label class="form-label" for="ano">Año de publicación:</label>
