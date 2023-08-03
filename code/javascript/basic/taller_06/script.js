99f8c43a6e905c847f609fde3a35e1461ca9ea75
Alecerzea <85199063+Alecerzea@users.noreply.github.com>
2023-08-02 21:26:28 -0500

    main

---
+++ code/javascript/basic/taller_06/script.js
@@ -0,0 +1,8 @@
+let articulos = [
+    { nombre: 'TV Sony', modelo: 'KD-65X85J LA8', costo: 759 },
+    { nombre: 'Audifonos Inalambricos', modelo: 'WH-CH710N', costo: 130 },
+    { nombre: 'Auriculares inalámbricos con Noise Cancelling', modelo: 'WF1000XM4/BMUC', costo: 290.4 },
+    { nombre: 'Smart TV Samsung', modelo: '55" AU7000 UHD 4K', costo: 773 },
+    { nombre: 'Laptop Lenovo', modelo: 'Core i3 1ma, 1tb, 12gb, touch, 15 pulg', costo:525.67 },
+    { nombre: 'Laptop Dell', modelo: 'Core i7 11va, 512gb ssd, 16gb, tec iluminado', costo: 1067.67 }
+]
