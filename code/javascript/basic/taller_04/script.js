21381b6f7a1784dbc5c04b3bfa7ff2d1bddb868e
Alecerzea <85199063+Alecerzea@users.noreply.github.com>
2023-08-02 21:07:01 -0500

    main

---
+++ code/javascript/basic/taller_04/script.js
@@ -0,0 +1,20 @@
+let mi_carro= {
+    marca: 'Alejito',
+    modelo: 'Mijito',
+    anio: 2023
+    detalle_auto: function()
+    console.log('Auto:${this.marca}-${this.modelo},Año:${this.anio}')
+}
+
+console.log(mi_carro.marca)
+mi_carro.marca='Lauco'
+mi_carro.detalle_auto()
+
+function carro(marca, modelo, anio){
+    this.marca = marca
+    this.modelo = modelo
+    this.anio = anio
+    this.detalle_auto = function() {
+        console.log('Auto:${this.marca}-${this.modelo},Año:${this.anio}')
+    }
+}
