20656cd90e6e99955954b1fb882ab988c444bd2b
Alecerzea <85199063+Alecerzea@users.noreply.github.com>
2023-09-08 14:09:19 -0500

    main

---
+++ code/Practica de Autenticacion/src/config.js
@@ -0,0 +1,7 @@
+const config = {
+    PORT: 3002,
+    SECRET:'INEXCELSISDEO',
+    MONGODB_URI:'mongodb+srv://alecerzea:miguelalejandro@cluster0.fqzper0.mongodb.net/?retryWrites=true&w=majority'
+}
+
+module.exports = config
