49415e0f9a5ead8e91dd8b2e06455536ce45a606
Alecerzea <85199063+Alecerzea@users.noreply.github.com>
2023-09-20 23:12:39 -0500

    main

---
+++ code/Examen de MCervantes/scrypt2.js
@@ -0,0 +1,10 @@
+document.getElementById('publisher-tab').addEventListener('click', function() {
+    window.location.href = 'index.html';
+});
+document.getElementById('book-tab').addEventListener('click', function() {
+    // Handle click event
+});
+document.getElementById('publisher-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    // Handle form submission
+});
