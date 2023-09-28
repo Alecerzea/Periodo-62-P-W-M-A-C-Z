b8053da694ff7e6acbe2e503db8ab4c0a4191c18
Alecerzea <85199063+Alecerzea@users.noreply.github.com>
2023-09-27 19:46:47 -0500

    main

--- code/sustitutorio de MCervantes/controllers/tituloController.js
+++ code/sustitutorio de MCervantes/controllers/tituloController.js
@@ -1,13 +1,12 @@
 const Titulo = require('../models/Titulo');
 
-exports.getTitulos = async (req,res) => {
-    const titulos = await
-    Titulo.find().populate('docente');
-    res.send(titulos);
+exports.getTitulos = async (req, res) => {
+  const titulos = await Titulo.find().populate('docente');
+  res.send(titulos);
 };
 
-exports.createTitulo = async (req,res) => {
-    const titulo = new Titulo(req.body);
-    await titulo.save();
-    res.send(titulo);
+exports.createTitulo = async (req, res) => {
+  const titulo = new Titulo(req.body);
+  await titulo.save();
+  res.send(titulo);
 };
