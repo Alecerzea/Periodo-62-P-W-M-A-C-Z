907a050e4221249955d3cce8ee07cb3977c734cc
Alecerzea <85199063+Alecerzea@users.noreply.github.com>
2023-09-27 19:17:29 -0500

    main

--- code/sustitutorio de MCervantes/apps.js
+++ code/sustitutorio de MCervantes/apps.js
@@ -1,1 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const docenteRoutes = require('./routes/docenteRoutes');
+const tituloRoutes = require('./routes/tituloRoutes');
+
+const app = express();
+
+mongoose.connect('mongodb://<usuario>:<contraseña>@cluster0.mongodb.net/<nombre-de-tu-base-de-datos>', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.use(bodyParser.json());
+app.use('/docentes', docenteRoutes);
+app.use('/titulos', tituloRoutes);
+
+app.listen(3000, () => console.log('Servidor iniciado en el puerto 3000'));
